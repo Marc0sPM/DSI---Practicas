3b0768ece00ca7770d1192c82b9a2d3525d037bf
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2024-04-11 16:58:00 +0200

    entra en el click pero no cambia la imagen

--- Assets/Lab5.cs
+++ Assets/Lab5.cs
@@ -7,7 +7,22 @@ using UnityEngine.UIElements;
 
 public class Lab5 : MonoBehaviour
 {
+    VisualElement nombre1;
+    VisualElement nombre2;
+    VisualElement personaje;
     private void OnEnable()
     {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        nombre1 = root.Q<VisualElement>("3DIcon");
+        nombre2 = root.Q<VisualElement>("16BitsIcon");
+        personaje = root.Q<VisualElement>("Personaje");
+        nombre1.RegisterCallback<ClickEvent>(CambiarIcono);
+        nombre2.RegisterCallback<ClickEvent>(CambiarIcono);
+    }
+
+    void CambiarIcono(ClickEvent evn)
+    {
+        VisualElement target = evn.target as VisualElement;
+        personaje.style.backgroundImage = target.style.backgroundImage;
     }
 }
